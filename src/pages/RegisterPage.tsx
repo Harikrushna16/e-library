d7bd77bfe565c15686b0f139644519c2ce515b82
Harikrushna16 <patelharikrushan8416@gmail.com>
2025-06-18 21:28:35 +0530

    added register/signup functionality

--- src/pages/RegisterPage.tsx
+++ src/pages/RegisterPage.tsx
@@ -1,3 +1,4 @@
+import { register } from "@/APIs/api";
 import { Button } from "@/components/ui/button";
 import {
   Card,
@@ -7,10 +8,43 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
+import useTokenStore from "@/store";
 import { Label } from "@radix-ui/react-label";
-import { Link } from "react-router-dom";
+import { useMutation } from "@tanstack/react-query";
+import { LoaderCircle } from "lucide-react";
+import React from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 const RegisterPage = () => {
+  const navigate = useNavigate();
+
+  const setToken = useTokenStore((state) => state.setToken);
+
+  const nameRef = React.useRef<HTMLInputElement>(null);
+  const emailRef = React.useRef<HTMLInputElement>(null);
+  const passwordRef = React.useRef<HTMLInputElement>(null);
+
+  const mutation = useMutation({
+    mutationFn: register,
+    onSuccess: (response: any) => {
+      setToken(response.data.accessToken);
+      navigate("/auth/login");
+    },
+  });
+
+  const handleRegisterSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    const name = nameRef.current?.value;
+    const email = emailRef.current?.value;
+    const password = passwordRef.current?.value;
+    if (!name || !email || !password) {
+      console.error("name, Email and password are required");
+      return;
+    }
+    console.log("Logging in with:", { email, password });
+    mutation.mutate({ name, email, password });
+  };
+
   return (
     <section className="flex justify-center items-center h-screen">
       <Card className="w-full max-w-sm">
@@ -19,17 +53,17 @@ const RegisterPage = () => {
           <CardDescription>
             Enter your information to create an account
           </CardDescription>
-          {/* {mutation.isError && (
+          {mutation.isError && (
             <span className="text-red-500 text-sm">
               {"Something went wrong"}
             </span>
-          )} */}
+          )}
         </CardHeader>
         <CardContent>
           <div className="grid gap-6">
             <div className="grid gap-2">
               <Label htmlFor="name">Name</Label>
-              <Input id="name" placeholder="Max" required />
+              <Input id="name" placeholder="Max" ref={nameRef} required />
             </div>
             <div className="grid gap-2">
               <Label htmlFor="email">Email</Label>
@@ -37,15 +71,21 @@ const RegisterPage = () => {
                 id="email"
                 type="email"
                 placeholder="m@example.com"
+                ref={emailRef}
                 required
               />
             </div>
             <div className="grid gap-2">
               <Label htmlFor="password">Password</Label>
-              <Input id="password" type="password" />
+              <Input id="password" type="password" ref={passwordRef} />
             </div>
 
-            <Button className="w-full">
+            <Button
+              className="w-full"
+              onClick={handleRegisterSubmit}
+              disabled={mutation.isPending}
+            >
+              {mutation.isPending && <LoaderCircle className="animate-spin" />}
               <span className="ml-2">Create an account</span>
             </Button>
           </div>
